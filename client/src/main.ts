3d6cfc67769b67967247099b59bf23b1b801dcf5
robertlknapp <robertlknapp@outlook.com>
2022-10-21 04:03:04 -0400

    Troubleshooting deployment

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//import { library } from '@fortawesome/fontawesome-svg-core'
+//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //import { faPersonWalking, faUserGroup, faAngleDown, faHeading, faCalendar, faClock, faLocationPin, faCamera, faUserSlash, faUserPen } from '@fortawesome/free-solid-svg-icons'
 
 //library.add(faPersonWalking, faUserGroup, faAngleDown, faHeading, faCalendar, faClock, faLocationPin, faCamera, faUserSlash, faUserPen)
@@ -17,5 +17,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.component('font-awesome-icon', FontAwesomeIcon)
+//app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
